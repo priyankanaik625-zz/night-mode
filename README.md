bdb229fbb134133970603acf72440184eb40a657
Priyanka Naik <priyankanaik@Priyankas-MacBook-Pro.local>
2019-06-23 14:11:52 +0200

    all three casses for day and night mode

---
+++ README.md
@@ -0,0 +1,10 @@
+Switch Between day and Night Theme
+
+Case 1:
+Switch to night mode on click of a button
+
+Case 2:
+Switch to night mode if it is night
+
+Case 3:
+Switch to night mode if OS is in the night mode
--- main.js
+++ main.js
@@ -1,66 +1,18 @@
-// var nightModeButton = document.querySelector(".night-mode__button");
-// var nightMode = document.querySelector(".night-mode");
+//Switch Between Day and night Theme
+var themeSwitch = document.getElementById("themeSwitch");
 
-// nightModeButton.addEventListener("click", () => {
-// 	nightMode.classList.toggle("night-mode--active");
-// });
+themeSwitch.addEventListener("change", event => {
+	event.target.checked
+		? document.body.setAttribute("data-theme", "dark")
+		: document.body.removeAttribute("data-theme");
+});
 
-// Dark mode
-const activateDarkMode = () => {
-	const rootElement = document.querySelector(":root");
-	const darkTheme = {
-		"--background-color": "#000000",
-		"--color": "#ffffff",
-		"--green": "#FFC0CB"
-	};
-	for (k in darkTheme) {
-		rootElement.style.setProperty(k, darkTheme[k]);
-	}
-	console.log("this is dark mode");
+// Switch to dark theme depending upon day and night
+var isNight = () => {
+	var date = new Date();
+	return date.getHours() > 22 || date.getHours() < 6;
 };
 
-// Light Mode
-const activateLightMode = () => {
-	const rootElement = document.querySelector(":root");
-	const lightTheme = {
-		"--background-color": "#ffffff",
-		"--color": "#000000",
-		"--green": "#008000"
-	};
-	for (k in darkTheme) {
-		rootElement.style.setProperty(k, lightTheme[k]);
-	}
-	console.log("this is Light mode");
-};
-
-const setColorScheme = () => {
-	const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-	const isLightMode = window.matchMedia("(prefers-color-scheme: light)")
-		.matches;
-	const isNotSpecified = window.matchMedia(
-		"(prefers-color-scheme: no-preference)"
-	).matches;
-	const hasNoSupport = !isDarkMode && !isLightMode && !isNotSpecified;
-
-	window
-		.matchMedia("(prefers-color-scheme: dark)")
-		.addEventListener(e => e.matches && activateDarkMode());
-	window
-		.matchMedia("(prefers-color-scheme: light)")
-		.addEventListener(e => e.matches && activateLightMode());
-
-	if (isDarkMode) activateDarkMode();
-	if (isLightMode) activateLightMode();
-
-	if (isNotSpecified || hasNoSupport) {
-		console.log("you have not specified a color scheme");
-
-		const now = new Date();
-		const hour = now.getHours();
-		if (hour < 4 || hour >= 16) {
-			activateDarkMode();
-		}
-	}
-};
-
-setColorScheme();
+isNight()
+	? document.body.setAttribute("data-theme", "dark")
+	: document.body.removeAttribute("data-theme");
