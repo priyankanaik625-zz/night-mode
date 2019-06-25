8e145a4d2e68defb3d480078c13289656e29028a
Priyanka Naik <priyankanaik@Priyankas-MacBook-Pro.local>
2019-06-25 11:16:10 +0200

    updated night mode to 20 uhr

--- main.js
+++ main.js
@@ -10,8 +10,7 @@ themeSwitch.addEventListener("change", event => {
 // Switch to dark theme depending upon day and night
 const isNight = () => {
 	var date = new Date();
-	console.log(date.getHours());
-	return date.getHours() > 10 || date.getHours() < 6;
+	return date.getHours() > 20 || date.getHours() < 6;
 };
 
 if (isNight()) {
